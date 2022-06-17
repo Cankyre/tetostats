d82ff7e4d6442d6d4056424e60c94be71101e5df
Cankyre <xilefznokan@gmail.com>
2022-06-17 13:50:40 +0200

    Changed stuff

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "",
   },
-  plugins: [svelte()]
+  plugins: [svelte(), viteSingleFile()]
 })
